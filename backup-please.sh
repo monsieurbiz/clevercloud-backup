1438079a3143969651fc99926533a56f37d9b8cf
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2023-02-24 19:34:36 +0100

    fix: Restart instead of start

--- backup-please.sh
+++ backup-please.sh
@@ -9,4 +9,4 @@ fi
 clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
 clever link --alias _backup_app $BACKUP_APP
 clever env --alias _backup_app set BACKUP_PLEASE_MY_LOVELY_SCRIPT true
-clever start --quiet --alias _backup_app
+clever restart --quiet --alias _backup_app
