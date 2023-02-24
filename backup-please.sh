e55e9bc5cf28b6375ad8928c833eebe2ddce7ef4
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2023-02-24 21:06:16 +0100

    feat: Run the backup-please.sh from Home

--- backup-please.sh
+++ backup-please.sh
@@ -6,6 +6,8 @@ if [[ "${INSTANCE_NUMBER}" != "0" ]]; then
     return 0;
 fi
 
+cd $APP_HOME
+
 clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
 clever link --alias _backup_app $BACKUP_APP
 clever env --alias _backup_app set BACKUP_PLEASE_MY_LOVELY_SCRIPT true
