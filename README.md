e7867e0142abb7a9f2b150ad33bac1660715e2c0
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2023-11-27 15:03:08 +0100

    feat: add backup script with kopia

--- README.md
+++ README.md
@@ -46,6 +46,13 @@ The backup application also has access to those variables through the linked Cel
 - `CELLAR_ADDON_KEY_ID`
 - `CELLAR_ADDON_KEY_SECRET`
 
+**For backup with kopia**
+
+```
+clever env -a backup set CC_PRE_RUN_HOOK ./clevercloud/pre_run_hook.sh
+clever env -a backup set CC_RUN_COMMAND ./backup-kopia.sh
+```
+
 ## How to run the backup itself?
 
 You need to copy the `backup-please.sh` script into your own main application.
---
+++ clevercloud/functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+set -o errexit -o nounset -o xtrace
+
+function install_dependencies() {
+    mkdir -p ${HOME}/.local/bin
+
+    # Download and extract kopia
+    wget https://github.com/kopia/kopia/releases/download/v0.15.0/kopia-0.15.0-linux-x64.tar.gz
+
+    # Verify that file checksums are ok
+    # wget https://github.com/kopia/kopia/releases/download/v0.15.0/checksums.txt
+    # sha256sum --check checksums.txt
+
+    # Import official signing key
+    # curl https://kopia.io/signing-key | gpg --import -
+    # # verify signature file
+    # wget https://github.com/kopia/kopia/releases/download/v0.15.0/checksums.txt.sig
+    # gpg --verify checksums.txt.sig
+
+    # extract kopia and install it
+    tar -xvf kopia-0.15.0-linux-x64.tar.gz
+    mv kopia-0.15.0-linux-x64/kopia ${HOME}/.local/bin/kopia
+    chmod +x ${HOME}/.local/bin/kopia
+}
+export -f install_dependencies
