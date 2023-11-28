d92dbc07a924dcc54ab636239dc256162b99dadf
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2023-11-28 11:11:06 +0100

    feat: add env variable to choose the kopia version

--- clevercloud/functions.sh
+++ clevercloud/functions.sh
@@ -3,24 +3,25 @@
 set -o errexit -o nounset -o xtrace
 
 function install_dependencies() {
+    KOPIA_VERSION="${KOPIA_VERSION:-0.15.0}"
     mkdir -p ${HOME}/.local/bin
 
     # Download and extract kopia
-    wget https://github.com/kopia/kopia/releases/download/v0.15.0/kopia-0.15.0-linux-x64.tar.gz
+    wget https://github.com/kopia/kopia/releases/download/v${KOPIA_VERSION}/kopia-${KOPIA_VERSION}-linux-x64.tar.gz
 
     # Verify that file checksums are ok
-    wget https://github.com/kopia/kopia/releases/download/v0.15.0/checksums.txt
+    wget https://github.com/kopia/kopia/releases/download/v${KOPIA_VERSION}/checksums.txt
     sha256sum --check checksums.txt --ignore-missing
 
     # Import official signing key
     curl https://kopia.io/signing-key | gpg --import -
     # Verify signature file
-    wget https://github.com/kopia/kopia/releases/download/v0.15.0/checksums.txt.sig
+    wget https://github.com/kopia/kopia/releases/download/v${KOPIA_VERSION}/checksums.txt.sig
     gpg --verify checksums.txt.sig
 
     # extract kopia and install it
-    tar -xvf kopia-0.15.0-linux-x64.tar.gz
-    mv kopia-0.15.0-linux-x64/kopia ${HOME}/.local/bin/kopia
+    tar -xvf kopia-${KOPIA_VERSION}-linux-x64.tar.gz
+    mv kopia-${KOPIA_VERSION}-linux-x64/kopia ${HOME}/.local/bin/kopia
     chmod +x ${HOME}/.local/bin/kopia
 }
 export -f install_dependencies
